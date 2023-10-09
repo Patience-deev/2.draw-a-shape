7c4cdbb1ded29a0a9631c4c07a352dd82d6e831f
Dell-User <Dell-User@DESKTOP-78SJPVI>
2023-10-09 10:53:32 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.draw_a_shape
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("      /|");
+            Console.WriteLine("     / |");
+            Console.WriteLine("    /  |");
+            Console.WriteLine("   /   |");
+            Console.WriteLine("   -----");
+
+            //freeze console
+            Console.ReadLine();
+        }
+    }
+}
